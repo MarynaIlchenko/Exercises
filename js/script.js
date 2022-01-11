283c07bb8cb038425c4acc9ab3fc24bede6ca959
MarynaIlchenko <debovamail@gmail.com>
2022-01-11 11:09:03 +0200

    every (task)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//---------------------forEach----------------------------------//
 //*Дан массив с числами. Создайте новый массив, состоящий из квадратов этих чисел*//
 // const numbers = [8, 2, 13, 5, 6, 20];
 // let newNumbers = [];
@@ -7,9 +8,26 @@
 // alert(newNumbers)
 
 //  Дан массив с числами. Найдите сумму этих чисел.//
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let totalSumArr = 0;
-arr.forEach(function (elem) {
-    totalSumArr += elem;
-});
-alert(totalSumArr)
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let totalSumArr = 0;
+// arr.forEach(function (elem) {
+//     totalSumArr += elem;
+// });
+// alert(totalSumArr)
+
+// --------------------Map-------------------------
+// Дан массив с числами. Сделайте из него массив, состоящий из квадратов этих чисел.
+// const arr = [2, 3, 5, 6, 9];
+// let newArr = [];
+// arr.map(function (elem) {
+//     newArr.push(elem * elem);
+// });
+// alert(newArr);
+
+// ------------- every, some ------------------------
+// Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля.
+const arr = [1, 20, 33, 32, 21, 0];
+function biggerThenNull(element, index, array) {
+    return element > 0;
+};
+[1, 20, 33, 32, 21, 0].every(biggerThenNull);
