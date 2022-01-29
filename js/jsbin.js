351307e53a2e62d1c58fd6914c15f11cb9f0cd58
MarynaIlchenko <debovamail@gmail.com>
2022-01-29 19:59:18 +0200

    task 2

--- js/jsbin.js
+++ js/jsbin.js
@@ -1,35 +1,91 @@
-// What the result?
-(function() {
-    var foo = new Object();
-    var bar = new Object();
-    var map = new Object();
+//  1 - What the result?
+// (function() {
+//     var foo = new Object();
+//     var bar = new Object();
+//     var map = new Object();
 
-    map[foo] = 'foo';
-    map[bar] = 'bar';
+//     map[foo] = 'foo';
+//     map[bar] = 'bar';
 
-    return map[foo];
-})();
+//     return map[foo];
+// })();
+
+// // Explanation 
+// var result = (function () {
+//     var foo = new Object(); // {}
+//     var bar = new Object(); // {}
+//     var map = new Object(); // {}
+
+//     map[foo] = "foo"; // map["[object Object] "] = "foo"
+//     map[bar] = "bar"; // map["[object Object] "] = "bar"
+  
+//   // Behind the scenes the key the toString method is called for object keys, so:
+//   // {}.toString() = "[object Object]"
+  
+//   console.log({}.toString()); // "[object Object]"
+  
+//   // Notice that only one key is returned because of the same key name:
+//   console.log(Object.keys(map)); // ["[object Object]"]
+
+//     // map["[object Object]"]
+//     return map[foo];
+// })();
+
+
+// console.log(result); // "bar"
+
+// ---2- --What the result?
+// function f() {
+//     return 'foo';
+// }
+// (function() {
+//     if (1 === 0) {
+//         function f() {
+//             return 'bar';
+//         }
+//     }
+//     return f();
+// })();
+// A.
+
+// "bar"
 
 // Explanation 
-var result = (function () {
-    var foo = new Object(); // {}
-    var bar = new Object(); // {}
-    var map = new Object(); // {}
+// var result = (function() {
+//     function f() {
+//         return 'foo';
+//     }
+//     return (function() {
+//         // JavaScript does not have block scope, only function scope. So the function below gets hoisted to the top of the function.
+//         if (1 === 0) {
+//             function f() {
+//                 return 'bar';
+//             }
+//         }
+//         // Now the function that gets called is the one within this environment, so "bar" gets returned. 
+//         return f();
+//     })();
 
-    map[foo] = "foo"; // map["[object Object] "] = "foo"
-    map[bar] = "bar"; // map["[object Object] "] = "bar"
-  
-  // Behind the scenes the key the toString method is called for object keys, so:
-  // {}.toString() = "[object Object]"
-  
-  console.log({}.toString()); // "[object Object]"
-  
-  // Notice that only one key is returned because of the same key name:
-  console.log(Object.keys(map)); // ["[object Object]"]
+// })();
 
-    // map["[object Object]"]
-    return map[foo];
-})();
+// console.log(result); // "bar"
 
+// // Demonstration of the hoisting that happens behind the scenes:
+// var result = (function() {
+//     function f() {
+//         return 'foo';
+//     }
+//     return (function() {
+//         function f() {
+//           return 'bar';
+//         }
+//         if (1 === 0) {
 
-console.log(result); // "bar"
+//         }
+//         return f();
+//     })();
+
+// })();
+
+// console.log(result); // "bar"
+
