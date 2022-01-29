31aaeb2d6b5849ea40d688983aadfc4f90445329
MarynaIlchenko <debovamail@gmail.com>
2022-01-29 17:16:45 +0200

    update

---
+++ js/jsbin.js
@@ -0,0 +1,35 @@
+// What the result?
+(function() {
+    var foo = new Object();
+    var bar = new Object();
+    var map = new Object();
+
+    map[foo] = 'foo';
+    map[bar] = 'bar';
+
+    return map[foo];
+})();
+
+// Explanation 
+var result = (function () {
+    var foo = new Object(); // {}
+    var bar = new Object(); // {}
+    var map = new Object(); // {}
+
+    map[foo] = "foo"; // map["[object Object] "] = "foo"
+    map[bar] = "bar"; // map["[object Object] "] = "bar"
+  
+  // Behind the scenes the key the toString method is called for object keys, so:
+  // {}.toString() = "[object Object]"
+  
+  console.log({}.toString()); // "[object Object]"
+  
+  // Notice that only one key is returned because of the same key name:
+  console.log(Object.keys(map)); // ["[object Object]"]
+
+    // map["[object Object]"]
+    return map[foo];
+})();
+
+
+console.log(result); // "bar"
