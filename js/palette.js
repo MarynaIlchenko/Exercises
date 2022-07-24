016295a16b3eba79323f3fdda6e8c5c1256f5e73
MarynaIlchenko <debovamail@gmail.com>
2022-07-24 17:49:26 +0300

    last update

--- js/palette.js
+++ js/palette.js
@@ -1,42 +1,43 @@
-const colorPalette = document.querySelector(".color-palette");
-const output = document.querySelector(".output");
+const colorPalette = document.querySelector('.color-palette')
+const output = document.querySelector('.output')
 
-colorPalette.addEventListener("click", selectColor);
+colorPalette.addEventListener('click', selectColor)
 
 // This is where delegation «magic» happens
 function selectColor(event) {
-  if (event.target.nodeName !== "BUTTON") { //Для проверки типа элемента используем свойство nodeName.
-    return;
-  }
+    if (event.target.nodeName !== 'BUTTON') {
+        //Для проверки типа элемента используем свойство nodeName.
+        return
+    }
 
-  const selectedColor = event.target.dataset.color;
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
+    const selectedColor = event.target.dataset.color
+    output.textContent = `Selected color: ${selectedColor}`
+    output.style.color = selectedColor
 }
 
 // Some helper functions to render palette items
-createPaletteItems();
+createPaletteItems()
 
 function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 60; i++) {
-    const color = getRandomColor();
-    const item = document.createElement("button");
-    item.type = "button";
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add("item");
-    items.push(item);
-  }
-  colorPalette.append(...items);
+    const items = []
+    for (let i = 0; i < 60; i++) {
+        const color = getRandomColor()
+        const item = document.createElement('button')
+        item.type = 'button'
+        item.dataset.color = color
+        item.style.backgroundColor = color
+        item.classList.add('item')
+        items.push(item)
+    }
+    colorPalette.append(...items)
 }
 
-function getRangomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+function getRandomColor() {
+    return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`
 }
 
 function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
+    return Math.round(Math.random() * 256)
+        .toString(16)
+        .padStart(2, '0')
 }
